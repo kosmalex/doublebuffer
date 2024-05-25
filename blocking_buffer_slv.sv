b900856f740aab8c4cfa4aeae70aaf8e63a10e4d
kosmalex <kosmas.alex22@gmail.com>
2024-05-25 19:23:53 +0300

    Buffers hadle axi_write channel only

--- blocking_buffer_slv.sv
+++ blocking_buffer_slv.sv
@@ -5,14 +5,8 @@ module blocking_buffer_slv #(
   input  logic                clk_i,
   input  logic                rst_n_i,
 
-  output logic                s_axi_awready_o,
-  input  logic                s_axi_awvalid_i,
-  input  logic [AXI_AW_g-1:0] s_axi_awaddr_i,
-  input  logic [7:0]          s_axi_awlen_i,
-  input  logic [2:0]          s_axi_awsize_i,
-  input  logic [1:0]          s_axi_awburst_i,
-  input  logic [2:0]          s_axi_awprot_i,  // Not used!
-  input  logic [3:0]          s_axi_awcache_i, // Not used!
+  input  logic                aw_burst_i,
+  input  logic                aw_size_i,
   
   output logic                s_axi_wready_o,
   input  logic                s_axi_wvalid_i,
@@ -20,18 +14,8 @@ module blocking_buffer_slv #(
   input  logic [3:0]          s_axi_wstrb_i,
   input  logic                s_axi_wlast_i,
 
-  input  logic                s_axi_bready_i,
-  output logic                s_axi_bvalid_o,
-  output logic [1:0]          s_axi_bresp_o,
-
   input  logic                grant_i,
-  input  logic [1:0]          aw_burst_type_i,
-  input  logic [2:0]          aw_size_i,
-  input  logic                continue_i,
-  output logic                available_o,
-  output logic                continue_o,
-  output logic [2:0]          aw_size_o,
-  output logic [1:0]          aw_burst_type_o
+  output logic                available_o
 );
 
 typedef enum logic[2:0] { IDLE, LOADING, PUSHING } buffer_state_t;
@@ -39,13 +23,7 @@ typedef enum logic[2:0] { IDLE, LOADING, PUSHING } buffer_state_t;
 buffer_state_t       buffer_state_s;
 
 logic [AXI_AW_g-1:0] aw_base_addr_s;
-logic [7:0]          aw_length_s;     // Not used!
-logic [2:0]          aw_size_s;
-logic [1:0]          aw_burst_type_s; // We use incremental only!
-
-logic                valid_aw_in_s;
 logic                axi_write_in_progress_s;
-
 logic [7:0]          element_counter_s;
 
 logic                a_wen_s;
@@ -82,7 +60,7 @@ always_ff @(posedge clk_i) begin
   end else begin
     case(buffer_state_s)
       IDLE: begin
-        buffer_state_s <= (valid_aw_in_s | continue_i) ? LOADING : IDLE;
+        buffer_state_s <= grant_i ? LOADING : IDLE;
       end
 
       LOADING: begin
@@ -108,25 +86,25 @@ always_ff @(posedge clk_i) begin
   end
 end
 
-assign valid_aw_in_s           = (s_axi_awvalid_i && s_axi_awready_o && grant_i);
-assign axi_write_in_progress_s = (buffer_state_s == LOADING) && 
-                                 (aw_burst_type_s == 2'b01 ) && 
-                                 (s_axi_wvalid_i           ) && 
-                                 (s_axi_wready_o           )  ;
+assign axi_write_in_progress_s = (buffer_state_s  == LOADING) && 
+                                 (aw_burst_i      == 2'b01  ) && 
+                                 (s_axi_wvalid_i            ) && 
+                                 (s_axi_wready_o            )  ;
 
 always_ff @(posedge clk_i) begin
   if (!rst_n_i) begin
-    { aw_base_addr_s,
-      aw_size_s,
-      aw_burst_type_s } <= 'd0;
+    aw_base_addr_s <= 'd0;
   end else begin
-    if ( (valid_aw_in_s | continue_i) && (buffer_state_s == IDLE) ) begin
-      aw_size_s       <= continue_i ? aw_size_i : s_axi_awsize_i;
-      aw_burst_type_s <= continue_i ? aw_burst_type_i : s_axi_awburst_i;
-      aw_base_addr_s  <= 'd0;                                    // Buffer always starts filling from 0x0
-    end else if (axi_write_in_progress_s) begin
-      aw_base_addr_s <= aw_base_addr_s + ('d1 << aw_size_s); // See ax_size encoding
+    if (axi_write_in_progress_s) begin
+      aw_base_addr_s <= aw_base_addr_s + ('d1 << aw_size_i); // See ax_size encoding
+    end else begin
+      aw_base_addr_s <= 'd0;                                 // Buffer always starts filling from 0x0
     end
+    // if ( grant_i && (buffer_state_s == IDLE) ) begin
+    //   aw_base_addr_s  <= 'd0;                                // Buffer always starts filling from 0x0
+    // end else if (axi_write_in_progress_s) begin
+    //   aw_base_addr_s <= aw_base_addr_s + ('d1 << aw_size_s); // See ax_size encoding
+    // end
   end
 end
 
@@ -137,21 +115,6 @@ always_ff @(posedge clk_i) begin
     element_counter_s <= element_counter_s + 'd1;
   end else if (buffer_state_s == PUSHING) begin
     element_counter_s <= element_counter_s - 'd1;
-  end
-end
-
-always_ff @(posedge clk_i) begin
-  if (!rst_n_i) begin
-    s_axi_bvalid_o <= 1'b0;
-  end else begin
-    if (
-      (buffer_state_s == LOADING) &&
-      s_axi_wlast_i && s_axi_wready_o
-    ) begin 
-      s_axi_bvalid_o <= 1'b1;
-    end else begin
-      s_axi_bvalid_o <= 1'b0;
-    end
   end
 end
 
@@ -167,15 +130,9 @@ assign b_addr_s = 'd15 - element_counter_s;
 assign a_data_in_s = s_axi_wdata_i;
 assign b_data_in_s = 0;
 
-assign continue_o      = (element_counter_s == 'd15) && s_axi_wready_o && (buffer_state_s == LOADING);
-assign available_o     = (buffer_state_s != PUSHING);
-assign aw_size_o       = aw_size_s;
-assign aw_burst_type_o = aw_burst_type_s;
+assign available_o = (buffer_state_s != PUSHING) && !( (element_counter_s == 'd15) && s_axi_wready_o );
 
 always_comb begin
-  s_axi_awready_o = (buffer_state_s == IDLE) & grant_i;
-  s_axi_bresp_o   = 'd0;
-
   if ( (buffer_state_s == LOADING) && (element_counter_s < 'd16) ) begin
     s_axi_wready_o = 1'b1;
   end else begin
--- double_buffer_slv.sv
+++ double_buffer_slv.sv
@@ -25,11 +25,17 @@ module double_buffer_slv #(
   output logic [1:0]          s_axi_bresp_o
 );
 
-logic       avail_buffer_s[2];
-logic       continue_buffer_s[2];
-logic [2:0] aw_size_s[2];
-logic [2:0] aw_burst_type_s[2];
-logic       grant_buffer_s[2];
+typedef enum logic[1:0] { IDLE, BUSY } dbuffer_state_t;
+
+dbuffer_state_t      st_s;
+
+logic                avail_buffer_s[2];
+logic                grant_buffer_s[2];
+logic                valid_aw_in_s;
+
+logic [2:0]          aw_size_s;
+logic [1:0]          aw_burst_type_s;
+logic [AXI_AW_g-1:0] aw_base_addr_s;
 
 logic                s_axi_awready_s[2];
 logic                s_axi_awvalid_s[2];
@@ -50,7 +56,7 @@ logic [1:0]          s_axi_bresp_s[2];
 
 generate
   genvar i;
-  for (i = 0; i < 2; i++) begin : buffer_module
+  for (i = 0; i < 2; i++) begin : blocking_buffer
     blocking_buffer_slv # (
       .AXI_DW_g(AXI_DW_g),
       .AXI_AW_g(AXI_AW_g)
@@ -59,35 +65,69 @@ generate
       .clk_i           (clk_i),
       .rst_n_i         (rst_n_i),
 
-      .s_axi_awready_o (s_axi_awready_s[i]),
-      .s_axi_awvalid_i (s_axi_awvalid_i),
-      .s_axi_awaddr_i  (s_axi_awaddr_i),
-      .s_axi_awlen_i   (s_axi_awlen_i),
-      .s_axi_awsize_i  (s_axi_awsize_i),
-      .s_axi_awburst_i (s_axi_awburst_i),
-      .s_axi_awprot_i  (s_axi_awprot_i),
-      .s_axi_awcache_i (s_axi_awcache_i),
+      .aw_burst_i      (aw_burst_type_s),
+      .aw_size_i       (aw_size_s),
+
       .s_axi_wready_o  (s_axi_wready_s[i]),
       .s_axi_wvalid_i  (s_axi_wvalid_i),
       .s_axi_wdata_i   (s_axi_wdata_i),
       .s_axi_wstrb_i   (s_axi_wstrb_i),
       .s_axi_wlast_i   (s_axi_wlast_i),
-      .s_axi_bready_i  (s_axi_bready_i),
-      .s_axi_bvalid_o  (s_axi_bvalid_s[i]),
-      .s_axi_bresp_o   (s_axi_bresp_s[i]),
       
       .grant_i         (grant_buffer_s[i]),
-      .aw_burst_type_i (aw_burst_type_s[1-i]),
-      .aw_size_i       (aw_size_s[1-i]),
-      .continue_i      (continue_buffer_s[1-i]),
-      .continue_o      (continue_buffer_s[i]),
-      .aw_size_o       (aw_size_s[i]),
-      .aw_burst_type_o (aw_burst_type_s[i]),
-
       .available_o     (avail_buffer_s[i])
     );
   end
 endgenerate
+
+always_ff @(posedge clk_i) begin
+  if (rst_n_i) begin
+    st_s <= IDLE;
+  end else begin
+    case(st_s)
+      IDLE: begin
+        st_s <= valid_aw_in_s ? BUSY : IDLE;
+      end
+      
+      BUSY: begin
+        st_s <= last_w_in_s ? IDLE : BUSY;
+      end
+
+      default: begin
+        st_s <= IDLE;
+      end
+    endcase
+  end
+end
+
+assign valid_aw_in_s   = (s_axi_awvalid_i && s_axi_awready_o);
+assign last_w_in_s     = (s_axi_wlast_i   && s_axi_wready_o);
+assign s_axi_awready_o = (st_s == IDLE);
+assign s_axi_bresp_o   = 'd0;
+
+always_ff @(posedge clk_i) begin
+  if (!rst_n_i) begin
+    { aw_size_s,
+      aw_burst_type_s } <= 'd0;
+  end else begin
+    if ( valid_aw_in_s && (st_s == IDLE) ) begin
+      aw_size_s       <= s_axi_awsize_i;
+      aw_burst_type_s <= s_axi_awburst_i;
+    end
+  end
+end
+
+always_ff @(posedge clk_i) begin
+  if (!rst_n_i) begin
+    s_axi_bvalid_o <= 1'b0;
+  end else begin
+    if ( (st_s == BUSY) && s_axi_wlast_i && s_axi_wready_o ) begin 
+      s_axi_bvalid_o <= 1'b1;
+    end else begin
+      s_axi_bvalid_o <= 1'b0;
+    end
+  end
+end
 
 always_comb begin
   if (avail_buffer_s[0]) begin
@@ -104,39 +144,17 @@ end
 
 always_comb begin
   if (avail_buffer_s[0]) begin
-    s_axi_awready_o    = s_axi_awready_s[0];
-    s_axi_awvalid_s[0] = s_axi_awvalid_i;
-    s_axi_awaddr_s[0]  = s_axi_awaddr_i;
-    s_axi_awlen_s[0]   = s_axi_awlen_i;
-    s_axi_awsize_s[0]  = s_axi_awsize_i;
-    s_axi_awburst_s[0] = s_axi_awburst_i;
-    s_axi_awprot_s[0]  = s_axi_awprot_i;
-    s_axi_awcache_s[0] = s_axi_awcache_i;
-    s_axi_wready_o     = s_axi_wready_s[0];
-    s_axi_wvalid_s[0]  = s_axi_wvalid_i;
-    s_axi_wdata_s[0]   = s_axi_wdata_i;
-    s_axi_wstrb_s[0]   = s_axi_wstrb_i;
-    s_axi_wlast_s[0]   = s_axi_wlast_i;
-    s_axi_bready_s[0]  = s_axi_bready_i;
-    s_axi_bvalid_o     = s_axi_bvalid_s[0];
-    s_axi_bresp_o      = s_axi_bresp_s[0];
+    s_axi_wready_o    = s_axi_wready_s[0];
+    s_axi_wvalid_s[0] = s_axi_wvalid_i;
+    s_axi_wdata_s[0]  = s_axi_wdata_i;
+    s_axi_wstrb_s[0]  = s_axi_wstrb_i;
+    s_axi_wlast_s[0]  = s_axi_wlast_i;
   end else begin
-    s_axi_awready_o    = s_axi_awready_s[1];
-    s_axi_awvalid_s[1] = s_axi_awvalid_i;
-    s_axi_awaddr_s[1]  = s_axi_awaddr_i;
-    s_axi_awlen_s[1]   = s_axi_awlen_i;
-    s_axi_awsize_s[1]  = s_axi_awsize_i;
-    s_axi_awburst_s[1] = s_axi_awburst_i;
-    s_axi_awprot_s[1]  = s_axi_awprot_i;
-    s_axi_awcache_s[1] = s_axi_awcache_i;
-    s_axi_wready_o     = s_axi_wready_s[1];
-    s_axi_wvalid_s[1]  = s_axi_wvalid_i;
-    s_axi_wdata_s[1]   = s_axi_wdata_i;
-    s_axi_wstrb_s[1]   = s_axi_wstrb_i;
-    s_axi_wlast_s[1]   = s_axi_wlast_i;
-    s_axi_bready_s[1]  = s_axi_bready_i;
-    s_axi_bvalid_o     = s_axi_bvalid_s[1];
-    s_axi_bresp_o      = s_axi_bresp_s[1];
+    s_axi_wready_o    = s_axi_wready_s[1];
+    s_axi_wvalid_s[1] = s_axi_wvalid_i;
+    s_axi_wdata_s[1]  = s_axi_wdata_i;
+    s_axi_wstrb_s[1]  = s_axi_wstrb_i;
+    s_axi_wlast_s[1]  = s_axi_wlast_i;
   end
 end
 
